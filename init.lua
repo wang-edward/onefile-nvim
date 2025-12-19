0bce1559a8fdd16b45942b161963a6c8742a9d74
Edward Wang <edawordwang@gmail.com>
2025-12-18 20:04:25 -0500

    ; lazy

--- init.lua
+++ init.lua
@@ -164,5 +164,6 @@ map('n', '<leader>n', '<cmd>vsplit | enew<cr>', { desc = 'new file split' })
 map('n', '<leader>v', '<cmd>split | enew<cr>', { desc = 'new file split vert' })
 map('n', 'L', '<cmd>BufferLineCycleNext<cr>')
 map('n', 'H', '<cmd>BufferLineCyclePrev<cr>')
+map('n', '<leader>;', '<cmd>Lazy<cr>')
 
 vim.cmd('colorscheme gruvbox')
