c6864edc974cc6bbffd5165e4c559645a0d577e7
Edward Wang <edawordwang@gmail.com>
2026-01-27 23:40:05 -0500

    comment out _ca cuz i dont use it and it slows down _c

--- init.lua
+++ init.lua
@@ -149,7 +149,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
         vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
         vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
         vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
-        vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+        -- vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
         vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
     end,
 })
