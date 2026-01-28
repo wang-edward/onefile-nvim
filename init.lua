0611df6a90604f4f01f0d0182c7d33d87e3c754d
Edward Wang <edawordwang@gmail.com>
2026-01-27 23:57:48 -0500

    golf

--- init.lua
+++ init.lua
@@ -94,13 +94,7 @@ require('lazy').setup({
                     },
                 }
             },
-            {
-                'hrsh7th/nvim-cmp',
-                dependencies = {
-                    'hrsh7th/cmp-nvim-lsp',
-                    'hrsh7th/cmp-buffer',
-                },
-            },
+            { 'hrsh7th/nvim-cmp', dependencies = { 'hrsh7th/cmp-nvim-lsp', 'hrsh7th/cmp-buffer', }, },
         }
     },
     { colorscheme = { 'gruvbox' } },
