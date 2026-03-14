4fed342c9c19b7e7d55abd7b43fc1f9842eb024e
Edward Wang <edawordwang@gmail.com>
2026-03-14 04:17:15 -0400

    rename codeaction to fix _c lagging

--- init.lua
+++ init.lua
@@ -154,7 +154,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
         vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
         vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
         vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
-        vim.keymap.set('n', '<leader>C', vim.lsp.buf.code_action, opts)
+        vim.keymap.set('n', '<leader>d', vim.lsp.buf.code_action, opts)
         vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
     end,
 })
